5939d4fb3ae055f602991fa9a9f61b2774413b8a
Yann Braga <yannbf@gmail.com>
2019-02-05 13:24:28 +0100

    feat: new mutations with mock data

--- src/index.js
+++ src/index.js
@@ -1,31 +1,83 @@
 import { ApolloServer } from 'apollo-server'
 import gql from 'graphql-tag'
 
+const users = [
+  {
+    id: 1,
+    username: 'Jean',
+    friends: [2]
+  },
+  {
+    id: 2,
+    username: 'Claude',
+    friends: [1]
+  },
+  {
+    id: 3,
+    username: 'Van Damme',
+    friends: []
+  },
+]
+
 // create typeDefs
 const typeDefs = gql`
   # type - Item (name string, done boolean)
   type Item {
-    name: String,
-    done: Boolean
+    name: String!
+    done: Boolean!
+  }
+
+  # type User (username string, items: [Item])
+  type User {
+    id: String
+    username: String!
+    friends: [User]
+  }
+
+  input NameInput {
+    name: String!
   }
 
   # create query - getItem
   type Query {
     getItem: Item
+    user(input: NameInput): User
+  }
+
+  # create input. Type can't be used as argument, so input is used for that.
+  input NewItemInput {
+    name: String!
   }
 
   # create mutation - createItem
   type Mutation {
-    createItem: Item
+    createItem(input: NewItemInput): Item!
   }
 `
 
 const resolvers = {
   Query: {
-    getItem: () => {},
+    getItem() {},
+    user(_, {input: {name} }) {
+      return users.find(user => user.username === name)
+    }
   },
   Mutation: {
-    createItem: () => {},
+    createItem(_, args, context, info) {
+      console.log(args.input)
+      console.log(JSON.stringify(info, null, 2))
+    },
+  },
+  User: {
+    friends(user) {
+      return users.filter(u => user.friends.includes === u.id)
+    },
+    username(user) {
+      return user.username
+    },
+    id(user) {
+      return user._id
+    }
   }
 }
 
---
+++ src/models.js
@@ -0,0 +1,16 @@
+// import mongoose from 'mongoose'
+
+// const UserSchema = new mongoose.Schema({
+//   username: String,
+//   {
+//     type: mongoose.Types.rela,
+//     ref: 'Item'
+//   }
+//  })
+
+// const ItemSchema = new mongoose.Schema({
+//   name: String,
+// })
+
+// export const User = mongoose.modelNames('User', UserSchema);
+// export const Item = mongoose.modelNames('Item', ItemSchema);
